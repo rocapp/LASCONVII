bc210445f9fa00a4288de5c0216e3bc0558ce0d3
Robert Capps <rocapp@gmail.com>
2018-01-24 17:50:17 -0200

    added version of model to Arnd

--- CMT/model/myelinatedModel_vRoth.py
+++ CMT/model/myelinatedModel_vRoth.py
@@ -1,31 +1,39 @@
+import os
 from neuron import h, gui
 from matplotlib import pyplot
 import numpy as np
-import os
 
-# PART 1: Active propagation Myelinated axon
+for aL in np.arange(7000,21000,2000):
 
-for aL in np.arange(100,300,10):
-
+	''' PART 1
+	Here we created a Neuron with 	a soma and an axon component
+	somaMotor is a sphere
+	axonMotor is a cylinder with 	length of 1 meter (10^6 microns)
+									segments of 2 microns each
+									each myelin sheet has a length of 48 microns
+									each ranvier node has a length of 02 microns
+	'''
+	# Creating the Soma structure
 	somaMotor = h.Section(name='somaMotor')
 	somaMotor.L = somaMotor.diam = 12.6157 # microns
 
+	# Creating the Axon structure
 	axonMotor = h.Section(name='axonMotor')
-	axonMotor.L = aL #30000 # microns
-	axonMotor.diam = 1 #microns
-	segmentSize = 50 # microns
-	axonMotor.nseg = int(aL / 2.0) #15000 # axonMotor.L/segmentSize
+	axonMotor.L = aL # microns
+	axonMotor.diam = 1. #microns
+	segmentSize = 2 # microns
+	print axonMotor.L/segmentSize
+	axonMotor.nseg = int(int(axonMotor.L) / int(segmentSize)) #15000 # axonMotor.L/segmentSize
 
+	# Connecting the Soma and Axon
 	axonMotor.connect(somaMotor(1))
 
+	# Changing the cell membrane resistance
 	for sec in h.allsec():
 		sec.Ra = 100    # Axial resistance in Ohm * cm
 		sec.cm = 1      # Membrane capacitance in micro Farads / cm^2
 
-	# print "Show the connection topology"
-	# print h.topology()
-
-	# Insert active Hodgkin-Huxley current in the soma
+	# Inserting active Hodgkin-Huxley current in the soma
 	somaMotor.insert('hh')
 	somaMotor.gnabar_hh = 0.12  # Sodium conductance in S/cm2
 	somaMotor.gkbar_hh = 0.036  # Potassium conductance in S/cm2
@@ -34,63 +42,45 @@ for aL in np.arange(100,300,10):
 
 	# Adding active HH channels in the Axon of the passive Neuron
 	axonMotor.insert('hh')
-	# axonMotor.gnabar_hh = 0.0  # Sodium conductance in S/cm2
-	# axonMotor.gkbar_hh = 0.0  # Potassium conductance in S/cm2
-	# axonMotor.gl_hh = 0.0    # Leak conductance in S/cm2
-	# axonMotor.cm = 0.01
 
-	ranvierNodeInterval = 1 # segmentSize/1000 # interval to place Nodes of Ranvier (in um)
-
-	for segIndex in range(1, axonMotor.nseg):#, ranvierNodeInterval):
+	for segIndex in range(1, axonMotor.nseg):
 		segIndexNorm = float(segIndex)/float(axonMotor.nseg)
 		
-		if segIndex % 25.0 == 0:
+		if segIndex % 25.0 == 0:						# Ranvier Node membrane properties
 			print segIndex
-			# print 'Okay!!'# statement(s)
-			# print (np.exp(-2*segIndex/axonMotor.nseg))*0.3
-
 			axonMotor(segIndexNorm).hh.gnabar = 0.12
 			axonMotor(segIndexNorm).hh.gkbar = 0.036
 			axonMotor(segIndexNorm).hh.gl = 0.0003
-			# axonMotor(segIndexNorm).pas.g = 0.001
-			axonMotor(segIndexNorm).cm = 5
+			axonMotor(segIndexNorm).cm = 5.
 
-		# 	axonMotor(segIndexNorm).hh.gnabar = (np.exp(-2*segIndex/axonMotor.nseg))*0.3
-		# 	print (1-np.exp(-2*segIndex/axonMotor.nseg))*0.3
-		# 	axonMotor(segIndexNorm).cm = (np.exp(-2*segIndex/axonMotor.nseg))*3
-		else:
-			axonMotor(segIndexNorm).hh.gnabar 	= 0.0  # Sodium conductance in S/cm2
-			axonMotor(segIndexNorm).hh.gkbar 	= 0.0  # Potassium conductance in S/cm2
-			axonMotor(segIndexNorm).hh.gl 		= 0.0    # Leak conductance in S/cm2
+		else:											# Myelin Sheat membrane properties
+
+			axonMotor(segIndexNorm).hh.gnabar 	= 0.0  	# Sodium conductance in S/cm2
+			axonMotor(segIndexNorm).hh.gkbar 	= 0.0  	# Potassium conductance in S/cm2
+			axonMotor(segIndexNorm).hh.gl 		= 0.0   # Leak conductance in S/cm2
 			axonMotor(segIndexNorm).cm = 0.01
+			'''	
+				This section creates an exponential degradation in the myelin sheat, 
+				by increasing the capacitance from the soma to the tip of the axon,
+				in order to mimic demyelination, but we are going to change it for another model later on
+			'''
 
 			# exponentialScaling = (np.exp(-5*(1-segIndexNorm)))
 			# # print exponentialScaling*0.12		
 			# channelScaling = 0.1
 			# axonMotor(segIndexNorm).hh.gnabar 	= exponentialScaling* 0.12*channelScaling # 0.0  # Sodium conductance in S/cm2
-			# axonMotor(segIndexNorm).hh.gkbar 	= exponentialScaling* 0.036*channelScaling # 0.0  # Potassium conductance in S/cm2
+			# axonMotor(segIndexNorm).hh.gkbar 		= exponentialScaling* 0.036*channelScaling # 0.0  # Potassium conductance in S/cm2
 			# axonMotor(segIndexNorm).hh.gl 		= exponentialScaling*0.0003 # 0.0    # Leak conductance in S/cm2
 			# axonMotor(segIndexNorm).cm 			= 0.01+exponentialScaling*(5-0.01)
 
-	# #Insert current clamp on 1 position of soma
+	# Insert current clamp on 1 position of soma
+	'''	
+		We tried current clamp, but it worked as same as the Alpha Synapse
+	'''
 	# stim = h.IClamp(somaMotor(0))
 	# stim.delay = 5
 	# stim.dur = 1
 	# stim.amp = 1
-
-	# stim1 = h.IClamp(somaMotor(0))
-	# stim1.delay = 55
-	# stim1.dur = 1
-	# stim1.amp = 1
-
-	# stim2 = h.IClamp(somaMotor(0))
-	# stim2.delay = 105
-	# stim2.dur = 1
-	# stim2.amp = 1
-
-	# axonMotor.insert('pas')
-	# # print h.psection(sec=axonMotor(0.5))
-	# axonMotor(0.5).g_pas = 0.001
 
 	# Insert Alpha Synapse
 	syn = h.AlphaSynapse(somaMotor(1.0))
@@ -99,7 +89,7 @@ for aL in np.arange(100,300,10):
 	syn.gmax = 0.1  # set conductance in uS
 	syn.tau = 0.1 # set time constant 
 
-	#set up plot
+	# Set up plot
 	t_vec = h.Vector()  # record time
 	t_vec.record(h._ref_t)
 
@@ -107,19 +97,16 @@ for aL in np.arange(100,300,10):
 	v_vec_soma.record(somaMotor(0.5)._ref_v)
 
 	# Setting up the recordings
-	axon_locs = np.arange(0.05,1,0.001)  # set axon recording times
+	axon_locs = np.arange(0.001,1,0.001)  # set axon recording times
 	v_vec_axon=[]
 	for loc in axon_locs:
 	    v_vec_axon.append(h.Vector())
 	    v_vec_axon[-1].record(axonMotor(loc)._ref_v)
 	    
 	## run simulation (passive propagation in axon)
-
-	#h.topology()
-
-	h.tstop = 75
+	# h.topology()
+	h.tstop = 500
 	h.run()
-        break
 
 	#pyplot format
 	pyplot.figure(figsize = (8,4))
@@ -127,9 +114,10 @@ for aL in np.arange(100,300,10):
 	for i,v_vec in enumerate(v_vec_axon):  # plot axon
 	    pyplot.plot(t_vec, v_vec, label = 'aM '+str(axon_locs[i]))
 	pyplot.legend()
-	pyplot.title('Degraded Axon - Stage 3 [10 cm]')
+	pyplot.title('Myelinated Axon: Axon Length: %d microns' % (aL,))
 	pyplot.xlabel('time (ms)')
 	pyplot.ylabel('mV')
-	pyplot.savefig(os.path.join('figs','axonLen_%d.png' % (aL,)))
+	pyplot.savefig('figs'+os.sep+'axonLen_%03d.png' % (aL,))
 	pyplot.close('all')
+	# pyplot.show()
 	
